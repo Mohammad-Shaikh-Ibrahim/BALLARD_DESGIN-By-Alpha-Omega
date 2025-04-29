8e73f5ff54b0ecd52df2a59557bb661dd315ed3d
Mohammad-Shaikh-Ibrahim <mohammadshaikhibrahim2002@gmail.com>
2025-04-29 16:54:16 +0300

    Finish The Header and Wall Art Section

---
+++ src/main.js
@@ -0,0 +1,33 @@
+fetch("data.json")
+  .then((response) => response.json())
+  // breadcrumbs
+  .then((data) => {
+    const breadcrumbs = data.wallArt.breadcrumbs;
+
+    const breadcrumbHTML = breadcrumbs
+      .map((item, index) => {
+        if (index === breadcrumbs.length - 1) {
+          return `<a id="basePosition" href="#">${item}</a>`;
+        } else {
+          return `<a class="bold" href="#">${item}</a>`;
+        }
+      })
+      .join(" > ");
+
+    document.getElementById("breadcrumbs").innerHTML = breadcrumbHTML;
+
+    // Categories
+    const categories = data.wallArt.categories;
+
+    const categoriesHTML = categories.map(category => {
+        return `
+          <div class="category">
+            <img src="${category.image}" alt="${category.name}">
+            <p>${category.name}</p>
+          </div>
+        `;
+      }).join("");
+
+      document.getElementById("categories").innerHTML = categoriesHTML;
+  })
+  .catch((error) => console.error("Error:", error));
