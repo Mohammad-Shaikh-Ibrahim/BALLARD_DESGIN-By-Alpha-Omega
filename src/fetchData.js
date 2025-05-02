3acf14a4c7d551d9f51c8c0774f590fc08a56dfb
Mohammad-Shaikh-Ibrahim <mohammadshaikhibrahim2002@gmail.com>
2025-05-02 10:44:56 +0300

    End Footer and Start With Product Section

--- src/fetchData.js
+++ src/fetchData.js
@@ -31,7 +31,30 @@ fetch("data.json")
       .join("");
 
     document.getElementById("categories").innerHTML = categoriesHTML;
+
+    // Products
+    const container = document.getElementById("productContainer");
+
+    const cards = data.products.map((product) => {
+      const card = document.createElement("div");
+      card.className = "product-card";
+
+      card.innerHTML = `
+        <img class="main-img" src="${product.image}" alt="${product.name}">
+        <div class="new-label">${product.isNew}</div>
+        <div class="thumbnails">
+          ${product.images
+            .map((img) => `<img src="${img}" alt="Thumbnail">`)
+            .join("")}
+        </div>
+        <div class="title">${product.name}</div>
+        <div class="old-price">${product.oldPrice}</div>
+        <div class="new-price">Sale ${product.newPrice}</div>
+      `;
+
+      return card;
+    });
+
+    cards.forEach((card) => container.appendChild(card));
   })
   .catch((error) => console.error("Error:", error));
-
-
